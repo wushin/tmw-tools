172d4fe7d6df51842f07db184df31a65a6f13ced
wushin <pasekei@gmail.com>
2014-03-27 23:00:24 -0500

    Changes to stop adding Extra end

--- tmx_converter.py
+++ tmx_converter.py
@@ -302,9 +302,10 @@ class ContentHandler(xml.sax.ContentHandler):
 
     def endDocument(self):
         self.mobs.write('\n\n%s.gat,0,0,0|script|Mob%s|-1,\n{\n' % (self.base, self.base))
+        self.mobs.write('    end;\n\n')
         for mob_id in sorted(self.mob_ids):
-            self.mobs.write('On%d:\n    set @mobID, %d;\n    callfunc "MobPoints";\n    end;\n\n' % (mob_id, mob_id))
-        self.mobs.write('    end;\n}\n')
+            self.mobs.write('On%d:\n    set @mobID, %d;\n    callfunc "MobPoints";\n    end;\n' % (mob_id, mob_id))
+        self.mobs.write('}\n')
         self.imports.write('// Map %s: %s\n' % (self.base, self.name))
         self.imports.write('// %s\n' % MESSAGE)
         self.imports.write('map: %s.gat\n' % self.base)
