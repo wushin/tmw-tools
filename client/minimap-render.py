0ba47cd91c32485591b7c91a2c1313d325f3687e
wushin <pasekei@gmail.com>
2014-09-17 20:37:36 -0500

    Map Alpha One

--- client/minimap-render.py
+++ client/minimap-render.py
@@ -16,7 +16,7 @@ CLIENT_DATA_ROOT = os.path.realpath(
 
 class MinimapRenderer(object):
 
-    MAP_RE = re.compile(r'^\d{3}-\d{1}(\.tmx)?$')
+    MAP_RE = re.compile(r'^\d{2}-\d{2}-\d{2}-\d{2}-\d{2}(\.tmx)?$')
     PROGRAMS = {
         'default': {
             'tmxrasterizer': 'tmxrasterizer',
